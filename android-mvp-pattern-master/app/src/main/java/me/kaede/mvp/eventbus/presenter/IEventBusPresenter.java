111724c87e2d611918284f129f6f25b6a6944232
zyd <qqlove521521521@126.com>
2017-01-23 15:01:58 +0800

    Revert "mvp"
    This reverts commit 3d631e55c3042fff6fb87349d0a8cec4d0001985.

--- android-mvp-pattern-master/app/src/main/java/me/kaede/mvp/eventbus/presenter/IEventBusPresenter.java
+++
@@ -1,9 +0,0 @@
-package me.kaede.mvp.eventbus.presenter;
-
-/**
- * Created by kaede on 2015/5/19.
- */
-public interface IEventBusPresenter {
-	public void loadDatas();
-	public void onItemClick(int position);
-}
--- android-mvp-pattern-master/app/src/main/java/me/kaede/mvp/home/presenter/IHomePresenter.java
+++
@@ -1,9 +0,0 @@
-package me.kaede.mvp.home.presenter;
-
-/**
- * Created by kaede on 2015/5/19.
- */
-public interface IHomePresenter {
-	public void loadDatas();
-	public void onItemClick(int position);
-}
